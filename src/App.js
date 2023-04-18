b9316d37774f79b4535a578a19b6a1d854cfbe1d
Ashish Chikane <87604972+AshishChikane@users.noreply.github.com>
2023-04-18 12:15:47 +0530

    login changes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@
 import React from 'react';
 import './App.css';
 import {Route,Routes} from 'react-router-dom'
-import IndexPage from './Components/pages/indexPage.jsx'
+import IndexPage from './Components/pages/indexPage.jsx';
+import LoginPage from './Components/pages/login'
+import Layout from './Components/Layout';
 
 
 function App() {
   return (
     <Routes>
-      <Route index element={<IndexPage />} />
+      <Route path="/" element={<Layout/>}> 
+      <Route index  element={<IndexPage />} />
+      <Route path="/login" element={<LoginPage/>} />
+      </Route>
     </Routes>
 
   );
---
+++ src/Components/pages/login.jsx
@@ -0,0 +1,11 @@
+export default function loginPage(){
+    return (
+        <div class >
+            <form>
+                <input type="email" placeholder="your@emailcom" />
+                <input type="password" placeholder="password" />
+                <button>Login</button>
+            </form>
+        </div>
+    );
+}
