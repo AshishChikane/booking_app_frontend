b3b1d26ccd7c517926e33d7d782aaba8ba9921bf
Ashish Chikane <87604972+AshishChikane@users.noreply.github.com>
2023-04-26 13:52:35 +0530

    registerPage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import React from 'react';
-import './App.css';
 import {Route,Routes} from 'react-router-dom'
 import IndexPage from './Components/pages/indexPage.jsx';
 import LoginPage from './Components/pages/login'
 import Layout from './Components/Layout';
+import Register from './Components/pages/register';
+import axios from 'axios';
 
+axios.defaults.baseURL = 'http://localhost:4000';
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
       <Route path="/" element={<Layout/>}> 
       <Route index  element={<IndexPage />} />
       <Route path="/login" element={<LoginPage/>} />
+      <Route path="/register" element={<Register/>}/>
       </Route>
     </Routes>
 
